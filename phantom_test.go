59d7665eafe1092a276fde796a030db0f79c5c22
Andreas Pfeil <Andreas.Pfeil@RaqUp.com>
2021-04-14 21:37:11 +0200

    Made the path to the phantomjs executeable configurable

--- phantom_test.go
+++ phantom_test.go
@@ -5,53 +5,53 @@ import (
 )
 
 func TestStartStop(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	err = p.Exit()
 	failOnError(err, t)
 }
 
 func TestStartStopWithArgs(t *testing.T) {
-	p, err := Start("--web-security=no")
+	p, err := Start("phantomjs", "--web-security=no")
 	failOnError(err, t)
 	err = p.Exit()
 	failOnError(err, t)
 }
 
 func TestRunACommand(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	defer p.Exit()
 	failOnError(err, t)
 	assertFloatResult("function(){ return 2 + 1; }\n", 3, p, t)
 }
 
 func TestRunACommandWithoutLineBreak(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	defer p.Exit()
 	failOnError(err, t)
 	assertFloatResult("function(){ return 2 + 2; }", 4, p, t)
 }
 
 func TestRunAnAsyncCommand(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p.Exit()
 	assertFloatResult("function(done){ done(2 + 3) ; }\n", 5, p, t)
-	p1, err := Start()
+	p1, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p1.Exit()
 	assertFloatResult("function(done){ setTimeout(function() { done(3 + 3) ; }, 0); }\n", 6, p1, t)
 }
 
 func TestRunMultilineCommand(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p.Exit()
 	assertFloatResult("function() {\n\t    return 3+4;\n}\n", 7, p, t)
 }
 
 func TestRunMultipleCommands(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p.Exit()
 	assertFloatResult("function() {return 1}", 1, p, t)
@@ -60,7 +60,7 @@ func TestRunMultipleCommands(t *testing.T) {
 }
 
 func TestLoadGlobal(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p.Exit()
 	p.Load("function result(result) { return result; }\nvar a = 2")
@@ -68,14 +68,14 @@ func TestLoadGlobal(t *testing.T) {
 }
 
 func TestMessageSentAfterAnErrorDontCrash(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p.Exit()
 	p.Run("function(done) {done(null, 'manual'); done('should not panic');}", nil)
 }
 
 func TestDoubleErrorSendDontCrash(t *testing.T) {
-	p, err := Start()
+	p, err := Start("phantomjs")
 	failOnError(err, t)
 	defer p.Exit()
 	p.Run("function(done) {done(null, 'manual'); done(null, 'should not panic');}", nil)
